a4118a3733fb43d0a18fb44feca627d9790121d1
Khaled gamal <khaled2000s.kg@gmail.com>
2022-11-18 15:57:35 +0200

    initial commit

---
+++ FundMe.sol
@@ -0,0 +1,36 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.9.0;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
+contract FundMe{
+
+    mapping(address => uint256) public fundMap;
+
+    //function that accepts payment
+    //payable: this function pays for things
+    function fund() public payable{
+        fundMap[msg.sender] += msg.value;
+    }
+
+    function getVersion() public view returns(uint256){
+        AggregatorV3Interface pricefeed = AggregatorV3Interface(0x639Fe6ab55C921f74e7fac1ee960C0B6293ba612);
+        return pricefeed.version();
+
+    }
+
+//returns the conversion price from eth to usd
+    function getPrice() public view returns(uint256){
+        AggregatorV3Interface pricefeed = AggregatorV3Interface(0x639Fe6ab55C921f74e7fac1ee960C0B6293ba612);
+        (,int256 answer,,,) =  pricefeed.latestRoundData();
+        return uint256(answer);
+    }
+
+    function getConversionRate(uint256 ethAmount) public view returns(uint256){
+        uint256 ethPrice = getPrice();
+        uint256 ethInUSD = (ethPrice * ethAmount) ;
+        return ethInUSD;
+    }
+
+
+}
